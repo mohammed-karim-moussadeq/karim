a402648ec77b8cc3088b03ed8dc535acc37354e6
101000755 <mohammedkarim.elmoussadeq@dcmail.ca>
2025-04-04 23:34:11 -0400

    Update main.js

--- assignment/part2/main.js
+++ assignment/part2/main.js
@@ -1,3 +1,6 @@
+/* Name; Mohammed Karim El Mousasdeq*/
+/*date: 04/04/2025*/
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
